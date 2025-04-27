a6957b3e8360f760b944319f5305ae1b608aef76
aokishofuan <shofuan.info@gmail.com>
2025-04-27 19:26:09 +0900

    Fix build errors

--- src/pages/orders.tsx
+++ src/pages/orders.tsx
@@ -1,6 +1,7 @@
 import { useEffect, useState } from 'react';
 import { Order } from '../types/Order';
 import { deleteOrder, subscribeOrders } from '@/lib/orderService';
+import { getDocs } from 'firebase/firestore';
 
 const OrdersPage = () => {
   const [orders, setOrders] = useState<Order[]>([]);
